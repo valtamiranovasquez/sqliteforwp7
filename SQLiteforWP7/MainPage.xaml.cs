cebda46c63a3f4fffd13c537ed51a5ca9a71362a
valtamiranovasquez <ppe_16@hotmail.com>
2014-05-03 11:12:43 -0500

    a

---
+++ SQLiteforWP7/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace SQLiteforWP7
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private   void btngrabar_Click(object sender, RoutedEventArgs e)
+        {
+            App.db.Insert(new Persona() { nombre  = txtnombre.Text});
+            MessageBox.Show("Guardado!");
+        }
+        private void btnlistar_Click(object sender, RoutedEventArgs e)
+        {
+            List<Persona> items = null;
+            items = App.db.Query<Persona>("select * from Persona ");
+            lista.ItemsSource = items;
+        }
+       
+
+         
+        private  void btnmodificar_Click(object sender, RoutedEventArgs e)
+        {
+            App.db.Update(new Persona() { Id = int.Parse(txtcodigo.Text), nombre = txtnombre.Text });
+            MessageBox.Show("Modifico!");
+        }
+
+        private void btneliminar_Click(object sender, RoutedEventArgs e)
+        {
+            App.db.Delete(new Persona() { Id = int.Parse(txtcodigo.Text) });
+            MessageBox.Show("Elimino!");
+        }
+       
+    }
+}
---
+++ SQLiteforWP7/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+namespace SQLiteforWP7
+{
+   public class Persona
+   {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(20)]
+        public string nombre { get; set; }
+    }
+}
